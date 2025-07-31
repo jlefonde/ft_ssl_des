bab2b81788e058453f5a6f78195c759a4af6e044
jlefonde <joris.lefondeur@protonmail.com>
2025-07-31 12:11:23 +0200

    Fixed errors caused by buffer out size

--- srcs/des_ecb.c
+++ srcs/des_ecb.c
@@ -16,7 +16,7 @@ void process_des_ecb(const t_command *cmd, int argc, char **argv)
     bool is_last_chunk = false;
     while (!is_last_chunk)
     {
-        if ((ctx->des.buffer.out_pos + 8) >= DES_BUFFER_SIZE * 2)
+        if ((ctx->des.buffer.out_pos + 8) >= DES_BUFFER_SIZE)
             write_des_output(cmd, ctx);
 
         size_t b64_offset = 0;
